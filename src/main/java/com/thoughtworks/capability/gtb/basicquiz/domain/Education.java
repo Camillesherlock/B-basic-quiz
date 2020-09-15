427e582fca9d62706c010ea934c4351490886818
Ke Liu <ke.liu1@thoughtworks.com>
2020-09-15 15:47:18 +0800

    [lk] add object User and Education

---
+++ src/main/java/com/thoughtworks/capability/gtb/basicquiz/domain/Education.java
@@ -0,0 +1,8 @@
+package com.thoughtworks.capability.gtb.basicquiz.domain;
+
+public class Education {
+    private Long userId;
+    private Long year;
+    private String title;
+    private String description;
+}
